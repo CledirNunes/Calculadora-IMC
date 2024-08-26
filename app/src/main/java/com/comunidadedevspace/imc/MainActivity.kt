bbe399d027af6097fbe7abeb4ad407ee9b7d2b93
nunes <nunescledir@gmail.com>
2024-08-26 12:23:10 +0200

    tela inicial edtesxt

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //recuperar componentes editext
+        //criar uma variavel e associar uma componente de UI<editext>
+        //recuperar botao da tela
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btncalcular = findViewById<Button>(R.id.btn_calcular)
+
+
+
+        btncalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+            println("cledir " + altura)
+        }
     }
+
+
 }
