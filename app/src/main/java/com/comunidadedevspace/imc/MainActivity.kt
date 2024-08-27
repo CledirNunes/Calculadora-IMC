ffbb3b0ba265451c7bbd0496e3e2c07499ca5fd3
nunes <nunescledir@gmail.com>
2024-08-27 11:58:11 +0200

    criando proxima pagina
    calculando imc
    
    mostrando resultado

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +22,34 @@ class MainActivity : AppCompatActivity() {
         val btncalcular = findViewById<Button>(R.id.btn_calcular)
 
         btncalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
-
-            val alturaQ2 = altura * altura
-            val resultado = peso / (alturaQ2)
 
 
-            println("cledir " + resultado)
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if(pesoStr == "" || alturaStr == ""){
+
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha todos os campos" ,
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            }else{
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+                val alturaQ2 = altura * altura
+                val resultado = peso / (alturaQ2)
+
+                //criando navegacao de telas
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("cledir"+ resultado)
+            }
+
         }
     }
 
